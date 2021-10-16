099e12a778a2ad5ca5674e2fc6ca94ad3a186bd8
ingong <insong1216@gmail.com>
2021-10-17 00:21:24 +0900

    첫 번째 프로젝트 - 할일 관리 애플리케이션

---
+++ quiz/1_todo/src/index.ts
@@ -0,0 +1,65 @@
+interface Todo {
+  id: number;
+  title: string;
+  done: boolean;
+}
+
+// 배열에 object 가 들어간다, 배열 리터럴로 선언하는 방식
+let todoItems: Todo[];
+
+// api
+function fetchTodoItems(): Todo[] {
+  const todos = [
+    { id: 1, title: '안녕', done: false },
+    { id: 2, title: '타입', done: false },
+    { id: 3, title: '스크립트', done: false },
+  ];
+  return todos;
+}
+
+// crud methods
+function fetchTodos(): Todo[] {
+  const todos = fetchTodoItems();
+  return todos;
+}
+
+function addTodo(todo: Todo): void {
+  todoItems.push(todo);
+}
+
+function deleteTodo(index: number): void {
+  todoItems.splice(index, 1);
+}
+
+function completeTodo(
+  index: number,
+  todo: { id: number; title: string; done: boolean }
+): void {
+  todo.done = true;
+  todoItems.splice(index, 1, todo);
+}
+
+// business logic
+function logFirstTodo(): object {
+  return todoItems[0];
+}
+
+function showCompleted(): Todo[] {
+  // TODO: 배열의 인자에도 속성값을 줘야하는 것 같다.
+  return todoItems.filter(item => item.done);
+}
+
+// TODO: 아래 함수의 내용을 채워보세요. 아래 함수는 `addTodo()` 함수를 이용하여 2개의 새 할 일을 추가하는 함수입니다.
+function addTwoTodoItems(): void {
+  addTodo({ id: 4, title: '안녕', done: false });
+  addTodo({ id: 5, title: '타입', done: false });
+}
+
+// NOTE: 유틸 함수
+function log(): void {
+  console.log(todoItems);
+}
+
+todoItems = fetchTodoItems();
+addTwoTodoItems();
+log();
